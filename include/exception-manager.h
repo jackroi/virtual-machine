42698096980a2262390f6a6532d550ede3b068c6
Giacomo <giacomorosin@gmail.com>
2019-05-16 22:04:01 +0200

    add exception-manger.c and exception-manager.h

---
+++ include/exception-manager.h
@@ -0,0 +1,18 @@
+/**
+ * exceptio-manager.h
+ * Copyright © 2019 Giacomo Rosin
+*/
+
+enum error {
+  MALLOC_ERROR = 1,
+  CANNOT_OPEN_FILE = 2,
+  INVALID_COMMAND = 3,
+  TOO_MANY_ARGS = 4,
+  NOT_ENOUGH_ARGS = 5,
+  NOT_IMPLEMENTED = 10        /* TODO probably to remove */
+};
+
+typedef enum error error_t;
+
+
+void log_error(error_t err_code);
---
+++ src/exception-manager.c
@@ -0,0 +1,12 @@
+/**
+ * exception-manager.c
+ * Copyright © 2019 Giacomo Rosin
+ *
+ * Logging functions
+*/
+
+
+#include "exception-manager.h"
+
+/**/
+void log_error(error_t err_code);
