7db14ab1c8632dfeef4e00060dee893a608deb0a
Giacomo <giacomorosin@gmail.com>
2019-05-18 15:48:14 +0200

    rewrite main and implement parse_command

--- src/main.c
+++ src/main.c
@@ -6,25 +6,47 @@
 */
 
 
-#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include "virtual-machine.h"
 
 
-int main(int argc, char const *argv[]) {
-  int error;
+static parse_command(const char *cmd);
 
-  /* // ? maybe reorder this if statement */
-  if (argc == 1) {
-    printf("ERROR #%d:\tnot implemented yet", 10);     /* TODO log error func */
-    error = 10;
-  } else if (argc == 3) {
-    /* TODO manage args transform argv[1] int an the code of the command */
-    int command = 1; // ! temp placeholder (? vm_parse_cmd() ?)
-    error = vm_run(command, argv[2]);
+
+int main(int argc, char const *argv[]) {
+  int error, command;
+
+  if (argc == 3) {
+    command = parse_command(argv[1]);
+
+    if (!command) {
+      error = INVALID_COMMAND;
+    } else {
+      vm_run(command, argv[2]);         /* start the virtual machine */
+    }
   } else {
-    printf("ERROR #%d:\tnumnber of arguments not correct (need 2 args)", 1);         /* TODO log error func */
-    error = 1;
+    error = ARGS_ERROR;
+  }
+
+
+  if (error) {
+    log_error(error);
   }
 
   return error;
+}
+
+
+static parse_command(const char *cmd) {
+  int c;
+  if (!strcmp(cmd, "print") || !strcmp(cmd, "stampa")) {
+    c = 1;
+  } else if (!strcmp(cmd, "print") || !strcmp(cmd, "stampa")) {
+    c = 2;
+  } else {
+    c = 0;
+  }
+
+  return c;
 }
