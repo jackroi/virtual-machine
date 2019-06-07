9bcc94850d61829e5de150b97fed664910f23a91
Giacomo <giacomorosin@gmail.com>
2019-06-07 16:12:26 +0200

    implement new error codes

--- include/exception-manager.h
+++ include/exception-manager.h
@@ -6,14 +6,24 @@
 #ifndef EXCEPTION_MANAGER_H
 #define EXCEPTION_MANAGER_H
 
+#define RETURN_IF_ERROR(expr, error) if (!expr) return error;
+
 enum error {
   NO_ERROR = 0,
   MALLOC_ERROR = 1,
   CANNOT_OPEN_FILE = 2,
   INVALID_COMMAND = 3,
   ARGS_ERROR = 4,
-  INVALID_CODE = 6,
-  NOT_IMPLEMENTED = 10        /* TODO probably to remove */
+  INVALID_CODE = 6,     /* TODO check if is covered by other errors */
+  INVALID_INSTRUCTION = 7,
+  INVALID_REG = 8,
+  OVERFLOW_ERROR = 9,
+  DIV_BY_ZERO = 10,
+  STACK_OVERFLOW = 11,
+  STACK_UNDEFLOW = 12,
+
+
+  NOT_IMPLEMENTED = 100        /* TODO probably to remove */
 };
 
 typedef enum error error_t;
